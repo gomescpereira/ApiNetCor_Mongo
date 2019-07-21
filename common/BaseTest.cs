ed38ec53fa8c313724bedc7313e61d76eb5328ea
gomescgcpereira <gomescgconsultoria@gmail.com>
2019-07-21 14:32:36 -0300

    teste.json

--- common/BaseTest.cs
+++ common/BaseTest.cs
@@ -1,15 +1,19 @@
 using mark7.Pages;
+using Microsoft.Extensions.Configuration;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 using System;
 using System.Drawing;
+using System.IO;
 using System.Threading;
 
 namespace mark7.Common
 {
     public class BaseTest 
     {
+        public static IConfigurationRoot Configuration { get; set;} 
         public IWebDriver driver;
         public LoginPage loginPage;
         public TaskPage taskPage;
@@ -17,10 +21,35 @@ namespace mark7.Common
         [SetUp]
         public void SetUp()
         {
-            driver = new ChromeDriver();
+            var builder = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("test.json");
+
+             Configuration = builder.Build();
+
+             var browser = Configuration["browser"];
+             var urlDefault = Configuration["url"];  
+
+             if (browser.Equals("chrome-headless"))
+             {
+                var options = new ChromeOptions();
+                options.AddArgument("--headless");
+                driver = new ChromeDriver(options);
+             } 
+             
+             if (browser.Equals("chrome"))
+             {
+                driver = new ChromeDriver();
+             }
+            
+            if (browser.Equals("fox"))
+            {
+                driver = new FirefoxDriver();
+            }
+            
 
             driver.Manage().Window.Size = new Size(1440, 900);
-            driver.Navigate().GoToUrl("http://192.168.1.15:5000/login");
+            driver.Navigate().GoToUrl(urlDefault);
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
 
             loginPage = new LoginPage(driver);
